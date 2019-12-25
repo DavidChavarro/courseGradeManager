6b0a8cf8ac41806c6ab5f4d603d20bf84f924b52
theTechOwl <58396635+theTechOwl@users.noreply.github.com>
2019-12-25 14:45:54 -0500

    Create Main.cpp

---
+++ src/Main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int Main(){
+cout << "If you see this message, it means the skeleton worked successfully.";
+return 0;//Terminates execution of program.
+}
