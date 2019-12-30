f67220f594c10db4d2cb4efbe5ff52a1fb25c36d
theTechOwl <theTechOwl@users.noreply.github.com>
2019-12-30 18:24:43 -0500

    Update Main.cpp
    Removed test message and added program's greeting message.

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,7 +1,11 @@
+//Executed as a driver c++ file. 
+//See readme or other classes' comments for more info.
+
 #include <iostream>
 using namespace std;
 
-int Main(){
-cout << "If you see this message, it means the skeleton worked successfully.";
-return 0;//Terminates execution of program.
+int main(){
+cout << "Welcome to the course calculator!\n";
+
+return 0;//Terminates execution of driver.
 }
